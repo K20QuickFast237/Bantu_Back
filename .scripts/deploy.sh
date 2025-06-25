bd36765f1f1e91708c3acd0881f95155e4608fbe
K20QuickFast237 <nanguedevops@gmail.com>
2025-06-25 15:23:33 +0100

    updated automation 2

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,14 +5,14 @@ echo "Deployment started ..."
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-(php artisan down) || true
+(sudo php artisan down) || true
 
 # Pull the latest version of the app
-sudo git pull origin main
+sudo git pull
 
 # Install composer dependencies
 # composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
-composer install
+composer install --no-interaction
 
 # Clear the old cache
 # php artisan clear-compiled
@@ -27,6 +27,6 @@ composer install
 php artisan migrate --force
 
 # Exit maintenance mode
-php artisan up
+sudo php artisan up
 
 echo "Deployment finished!"
