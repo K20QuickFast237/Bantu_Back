ba9d004f15b30bf8225032d1ac30f8820b11c26b
K20QuickFast237 <nanguedevops@gmail.com>
2025-06-29 14:47:16 +0100

    removed purpose in command instructions

--- routes/console.php
+++ routes/console.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+}); // ->purpose('Display an inspiring quote')
 
-Schedule::command ("auth:clear-resets")->purpose("Clear expired password resets")->daily();
-Schedule::command("passport:purge")->purpose('Purge expired tokens')->daily();
+Schedule::command ("auth:clear-resets")->daily();   // ->purpose("Clear expired password resets")
+Schedule::command("passport:purge")->daily();       // ->purpose('Purge expired tokens')
 
 /*
     Il ne reste plus qu'a confiurer sur le serveur un cron qui lancera la commande
