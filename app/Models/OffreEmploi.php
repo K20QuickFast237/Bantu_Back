5ad07c81db668bf23276eae3910a68c7db73bfe7
iamKSMO <michelsombang@gmail.com>
2025-10-20 15:26:56 -0700

    Update: creation des offres avec categorie

--- app/Models/OffreEmploi.php
+++ app/Models/OffreEmploi.php
@@ -14,6 +14,7 @@ class OffreEmploi extends Model
 
     protected $fillable = [
         'employeur_id',
+        'categorie_id',
         'titre_poste',
         'description_poste',
         'exigences',
