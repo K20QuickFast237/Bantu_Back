e33a53df567d17a60b3a72ce0dccc243727c46c0
K20QuickFast237 <nanguedevops@gmail.com>
2025-12-21 16:43:05 +0100

    test

--- app/Http/Requests/StoreOffreEmploiRequest.php
+++ app/Http/Requests/StoreOffreEmploiRequest.php
@@ -24,27 +24,27 @@ class StoreOffreEmploiRequest extends FormRequest
         return [
             // Étape 2 : infos principales
             'titre_poste' => 'required|string|max:255',
-            'lieu_travail' => 'nullable|string|max:255',
+            'lieu_travail' => 'required|string|max:255',
             'description_poste' => 'required|string',
-            'exigences' => 'nullable|string',
-            'responsabilites' => 'nullable|string',
-            'experience_requise' => 'nullable|required|string|in:<1an,1-3ans,4-5ans,>5ans',
+            'exigences' => 'required|string',
+            'responsabilites' => 'required|string',
+            'experience_requise' => 'sometimes|required|string|in:<1an,1-3ans,4-5ans,>5ans',
             'ville' => 'required|string|max:255',
             'pays' => 'required|string|max:255',
             'type_contrat' => 'required|in:CDI,CDD,Interim,Stage,Alternance,Freelance,Autre',
-            'remuneration_min' => 'nullable|numeric|min:0',
-            'remuneration_max' => 'nullable|numeric|gte:remuneration_min',
-            'document_annonce' => 'nullable|file|mimes:pdf,doc,docx|max:2048',
+            'remuneration_min' => 'sometimes|numeric|min:0',
+            'remuneration_max' => 'sometimes|numeric|gte:remuneration_min',
+            'document_annonce' => 'sometimes|file|mimes:pdf,doc,docx|max:2048',
 
             'date_publication' => 'required|date',
             'date_limite_soumission' => 'required|date|after_or_equal:date_publication',
 
             // Étape 3 : candidature
-            'email_candidature' => 'required|email|max:255',
-            'url_candidature' => 'nullable|url|max:255',
-            'instructions_candidature' => 'nullable|string',
+            'email_candidature' => 'sometimes|required|email|max:255',
+            'url_candidature' => 'sometimes|required|url|max:255',
+            'instructions_candidature' => 'sometimes|required|string',
             // 'experience_requise' => 'nullable|required|string',
-            'documents_requis' => 'nullable|array',
+            'documents_requis' => 'sometimes|array',
             'documents_requis.*' => 'string|max:255',
 
             // Étape 4 : état de l'offre
@@ -52,7 +52,7 @@ class StoreOffreEmploiRequest extends FormRequest
             'nombre_vues' => 'nullable|integer|min:0',
 
             // Skills (si tu gardes la relation)
-            'skills' => 'nullable|array',
+            'skills' => 'sometimes|array',
             'skills.*' => 'exists:skills,id',
 
             'categorie_id' => 'required|exists:offre_categories,id',
