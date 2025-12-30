9bd170f5557ca34876642e89e9896f88acfa959f
K20QuickFast237 <nanguedevops@gmail.com>
2025-12-30 17:40:15 +0100

    cors updated

--- config/cors.php
+++ config/cors.php
@@ -27,8 +27,12 @@ return [
         'http://127.0.0.1:5174',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://localhost:8080',
+        'http://127.0.0.1:8080',
         'http://localhost:8081',
         'http://127.0.0.1:8081',
+        'http://localhost:8082',
+        'http://127.0.0.1:8082',
         'https://bantulink.tech'
     ],
 
